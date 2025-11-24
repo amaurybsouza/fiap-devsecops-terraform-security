cb5b6f47d7a5c6da2ffe1de3a7320b548386b452
amaurybsouza <amaurybsouza@gmail.com>
2025-11-24 18:02:13 -0300

    remove file

--- main.tf
+++ main.tf
@@ -48,12 +48,3 @@ resource "aws_instance" "web" {
     Name = "HelloWorld"
   }
 }
-
-resource "aws_s3_bucket" "example" {
-  bucket = "my-tf-test-bucket"
-
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
-}
