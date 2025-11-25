619947005c830e9f4922eea93094fc5bfbdba690
amaurybsouza <amaurybsouza@gmail.com>
2025-11-24 21:45:53 -0300

    update file

--- main.tf
+++ main.tf
@@ -31,6 +31,7 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
+# create aws ec2
 resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t3.micro"
