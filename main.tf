ab1716b914d62bf29584f36010c7f729a6243cfb
amaurybsouza <amaurybsouza@gmail.com>
2025-11-24 21:49:08 -0300

    update terraform file

--- main.tf
+++ main.tf
@@ -31,7 +31,7 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
-# create aws ec2
+# create aws ec2 instance.
 resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t3.micro"
